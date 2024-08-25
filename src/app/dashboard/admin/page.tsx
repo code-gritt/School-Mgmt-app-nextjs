2710d14f0b559fbee852c61d2822374c770b97f7
gokul-saaragh <gokulchandan24@gmail.com>
2024-08-26 00:02:15 +0530

    created react calendar

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -30,8 +32,8 @@ const AdminPage = () => {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* <EventCalendar />
-        <Announcements /> */}
+        <EventCalendar />
+        <Announcements />
       </div>
     </div>
   );
