23f128e5021e4bb3b687a5c111e1b91c579873ef
gokul-saaragh <gokul@saaragh.com>
2024-08-25 23:56:46 +0530

    created rest of components

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,3 +1,5 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -16,11 +18,11 @@ const AdminPage = () => {
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            {/* <CountChart /> */}
+            <CountChart />
           </div>
           {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            {/* <AttendanceChart /> */}
+            <AttendanceChart />
           </div>
         </div>
         {/* BOTTOM CHART */}
